31c1c778b41e4d2fe3a872fe08b9f9b6337c5948
porna <koneporna204@gmail.com>
2026-03-04 01:49:42 +0000

    ajoute whitenoise

--- cryptosafe/settings.py
+++ cryptosafe/settings.py
@@ -34,7 +34,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'cryptosafe.urls'
 
